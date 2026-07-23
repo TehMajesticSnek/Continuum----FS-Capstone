e11e6e6956a703ebb5ffc026471296690d339d58
Lucas Mcdonough <lucasmcd0114@gmail.com>
2026-07-22 22:21:34 -0400

    Figuring out branches

--- Continuum/app/src/main/java/com/continuum/MainActivity.kt
+++ Continuum/app/src/main/java/com/continuum/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ContinuumTheme {
-        Greeting("Android")
+        Greeting("Gary")
     }
 }
